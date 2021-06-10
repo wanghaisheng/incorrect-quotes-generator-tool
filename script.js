4394bad41780219efd4d754909bb946aee5dd3ec
AndyThePie <12beesinatrenchcoat+gitstuff@protonmail.com>
2021-06-10 14:12:52 -0400

    :spaghetti: only enable inputs when previous input is filled
    this one's a bit of a nightmare in terms of code... hardcore spaghetti. like, *serious* spaghetti.
    
    but it works...!

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// getting prompts - they're in a separate .json file.
 let prompts;
 fetch("./prompts.json")
 	.then(response => response.json())
@@ -13,15 +14,54 @@ fetch("./prompts.json")
 		document.querySelector("#prompt-count").textContent = promptCount;
 	});
 
-window.onload = function () {
-};
+const test = document.querySelectorAll(".character");
+
+for (let i = 0; i < test.length; i++) {
+	test[i].disabled = true; // disable all inputs by default
+	test[i].value = ""; // also clear everything. no persistence.
+
+	// clear input on focus
+	test[i].onfocus = function () {
+		const inputIndex = i;
+		test[inputIndex].value = "";
+		console.log("index", inputIndex);
+		for (let i = inputIndex; i < test.length - 1; i++) {
+			test[i + 1].disabled = true;
+		}
+	};
+
+	// enable next input (oninput, so you can tab through inputs)
+	test[i].oninput = function () {
+		if (test[i].textLength > 0) {
+			if (i < test.length - 1) {
+				test[i + 1].disabled = false;
+			}
+		}
+	};
+
+	// re-enable input if input has content (disabled by clearing previous input)
+	test[i].onchange = function () {
+		const inputIndex = i;
+		for (let i = inputIndex; i < test.length - 1; i++) {
+			if (test[i + 1].textLength > 0) {
+				test[i + 1].disabled = false;
+			} else {
+				break;
+			}
+		}
+	};
+}
+
+test[0].disabled = false; // first input should never be disabled
 
 // eslint-disable-next-line no-unused-vars
 function generatePrompt() {
+	// get characters from <input>s, add to array
 	const characters = [];
-
 	for (const input of document.querySelectorAll(".character")) {
-		if (input.value !== "") {
+		if (input.value === "") {
+			break;
+		} else {
 			characters.push(input.value);
 		}
 	}
