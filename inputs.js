12ff6f7ad7600603b3665dc96132e81e17c5d5b1
AndyThePie <25379179+AndyThePie@users.noreply.github.com>
2021-08-11 03:56:53 -0400

    remove obsolete input functions the ones that relate to input functions, those are no longer used (can't be, really, structure of character inputs changed a lot).

--- inputs.js
+++ inputs.js
@@ -30,54 +30,6 @@ window.getCharacters = (() => {
 const style = document.createElement("style");
 document.head.append(style);
 
-// gives inputs their functions
-function giveInputFunctions() {
-	const inputs = window.getCharacters();
-
-	// clear input on focus
-	for (let i = 0; i < inputs.length - 1; i++) {
-		inputs[i].onfocus = function () {
-			const inputIndex = i;
-			inputs[inputIndex].value = "";
-			for (let i = inputIndex; i < inputs.length - 1; i++) {
-				inputs[i + 1].disabled = true;
-			}
-		};
-
-		// enable next input (oninput, so you can tab through inputs)
-		inputs[i].oninput = function () {
-			if (inputs[i].textLength > 0) {
-				if (i < inputs.length - 1) {
-					inputs[i + 1].disabled = false;
-				}
-			}
-		};
-
-		// re-enable input if input has content (disabled by clearing previous input)
-		inputs[i].onchange = function () {
-			const inputIndex = i;
-			for (let i = inputIndex; i < inputs.length - 1; i++) {
-				if (inputs[i + 1].textLength > 0) {
-					inputs[i + 1].disabled = false;
-				} else {
-					break;
-				}
-			}
-		};
-	}
-}
-
-function enableDisableInputs() {
-	const inputs = window.getCharacters();
-	for (let i = 1; i < inputs.length; i++) {
-		if (inputs[i - 1].textLength > 0) {
-			inputs[i].disabled = false;
-		} else {
-			inputs[i].disabled = true;
-		}
-	}
-}
-
 // this is the part where characters are added. this is where things get messy.
 window.addInput = function () {
 	const inputsDiv = document.querySelector("#character-blocks");
@@ -105,8 +57,6 @@ window.addInput = function () {
 	style.sheet.insertRule(`.char-${inputNumber} { background: ${color}C4 }`);
 
 	inputsDiv.append(charBlock);
-	giveInputFunctions(inputNumber - 1);
-	enableDisableInputs();
 };
 
 window.removeInput = function () {
